8ecc83cc7cf1c8f566ebdb06caaafb7f1c0d2643
matthewhudson <matthud@gmail.com>
2019-08-14 11:44:58 -0300

    Fixing ES6 exports + test.

---
+++ src/dom/attr.test.js
@@ -0,0 +1,27 @@
+import attr from './attr.js'
+// import domify from './domify'
+
+import test from 'ava'
+
+test('my passing test', t => {
+  t.pass()
+})
+
+/*
+describe('attr', function () {
+  it('should get attribute', function () {
+    var node = domify('<div title="FOO">')
+
+    expect(attr(node, 'title')).to.eql('FOO')
+  })
+
+  it('should set attribute', function () {
+    var node = domify('<div title="FOO">')
+
+    // when
+    attr(node, 'title', 'BAR')
+
+    expect(attr(node, 'title')).to.eql('BAR')
+  })
+})
+*/
